d8c578316d018b2abfc5754a5e1281b7bc69dda5
David <technologyguy417@gmail.com>
2025-06-10 17:04:09 +0500

    Intial commit

--- practice.py
+++ practice.py
@@ -1,22 +1,7 @@
-import tkinter as tk
-from tkinter import messagebox
-
-window = tk.Tk()
+from tkinter import *
+window = Tk()
 window.title('Password Manager')
 window.geometry('300x200')
-
-def show_text():
-    username = entry.get()
-    messagebox.showinfo('Hi', username)
-
-def clear_text():
-    entry.delete(0, tk.END)
-
-entry = tk.Entry(window, width=30)
-entry.pack(pady=10)
-
-show_button = tk.Button(window, text='Show Text', command=show_text)
-show_button.pack(pady=5)
-clear_button = tk.Button(window, text='Clear Text', command=clear_text)
-clear_button.pack(pady=5)
+label1 = Label(window, text='Hi, My name is David.').grid(row=0, column=0)
+label2 = Label(window, text='I am a Student.').grid(row=0, column=1)
 window.mainloop()
